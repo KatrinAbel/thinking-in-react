54f4109a08b9883351e99a8c51cfcba17195451f
Maxence Bouret <maxence.bouret@gmail.com>
2018-11-22 10:59:40 +0100

    Final iteration

--- src/components/FilterableProductTable.jsx
+++ src/components/FilterableProductTable.jsx
@@ -10,6 +10,16 @@ export default class FilterableProductTable extends Component {
       stocked: false
     }
   }
+  handleSearch = (newSearch) => {
+    this.setState({
+      search: newSearch
+    })
+  }
+  handleStocked = (newStocked) => {
+    this.setState({
+      stocked: newStocked
+    })
+  }
   render() {
     let upperSearch = this.state.search.toUpperCase()
     let filteredProducts = this.props.products
@@ -21,7 +31,11 @@ export default class FilterableProductTable extends Component {
       .sort((a,b) => (a.category > b.category ? 1 : -1))
     return (
       <div className="FilterableProductTable">
-        <SearchBar search={this.state.search} stocked={this.state.stocked} />
+        <SearchBar 
+          onSearch={this.handleSearch} 
+          onStocked={this.handleStocked}
+          search={this.state.search} 
+          stocked={this.state.stocked} />
         <ProductTable products={filteredProducts} />
       </div>
     )
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
-export default function SearchBar({search,stocked}) {
+export default function SearchBar({search,stocked,onSearch,onStocked}) {
   return (
     <div className="SearchBar">
       <form>
-        <input type="text" name="search" placeholder="Search" value={search} /> <br/>
-        <input type="checkbox" name="stocked" checked={stocked} /> is stocked
+        <input 
+          type="text" 
+          name="search" placeholder="Search" 
+          value={search} 
+          onChange={e => onSearch(e.target.value)} /> 
+          <br/>
+        <input 
+          type="checkbox" 
+          name="stocked" 
+          checked={stocked} 
+          onChange={e => onStocked(e.target.checked)}/> is stocked
       </form>
     </div>
   )
